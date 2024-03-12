1271bc7d726d330b9edc798aa76e7a602e4a72e3
Anil Parajuli <anilparajuli998@gmail.com>
2024-03-12 22:47:24 +0545

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
+mongoose.connect(process.env.MONGO).then(()=>console.log('database is connected')).catch((err)=>console.log('error', err))
 
 app.listen(3000, ()=>{
     console.log('server is running');
