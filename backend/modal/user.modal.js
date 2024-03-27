85005bb190a9589966528e766d1f65280ce90a91
Anil Parajuli <anilparajuli998@gmail.com>
2024-03-27 22:06:11 +0545

    add admin functionality to user

--- backend/modal/user.modal.js
+++ backend/modal/user.modal.js
@@ -18,7 +18,11 @@ const userSchema = new mongoose.Schema({
     profilepicture:{
         type:String,
         default:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fblank-profile-picture&psig=AOvVaw26mLA-A2BcS_XQQsGy6ho0&ust=1711170192304000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJijxu2Lh4UDFQAAAAAdAAAAABAE'
-    }
+    },
+    isAdmin:{
+        type:Boolean,
+        default: false,
+    },
 }, {timestamps: true}
 )
 
