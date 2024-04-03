9bc12b65a2df7db1d7cb21a2f255d93b25ee50c7
Anil Parajuli <anilparajuli998@gmail.com>
2024-04-03 14:51:17 +0545

    create get user api

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { Test, signout, updateUser } from '../controllers/user.controller.js'
+import { Test, getUsers, signout, updateUser } from '../controllers/user.controller.js'
 import { verifyToken } from '../utils/verifyUser.js'
 
 const router = express.Router()
@@ -7,5 +7,6 @@ const router = express.Router()
 router.get('/test', Test)
 router.put('/update/:userId',verifyToken, updateUser)
 router.post('/signout', signout)
+router.get('/getusers', verifyToken, getUsers)
  
 export default router  
